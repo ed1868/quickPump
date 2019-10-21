a5ca8cd0737a3f9b7f69c677b90ae6552466b827
eddieruiz <eddi@overseasinternational.com>
2019-10-21 17:18:06 -0400

    User signup complete

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  imgName: String,
+  height: String,
+  weight: String,
+  fitnessLevel: String,
+  goalWeight: String,
+  dueDate: String,
+  status: {
+    type: String,
+    enum: ['Pending Confirmation', 'Active'],
+    default: 'Pending Confirmation',
+  },
+  confirmationCode: {
+    type: String,
+    unique: true,
+
+  },
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
