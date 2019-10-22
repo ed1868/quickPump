68a3467ec8fabebc33776ecd2d9f1ed8f86c0d8e
eddieruiz <eddi@overseasinternational.com>
2019-10-22 16:34:08 -0400

    workouts all route complete- not tested

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const Workouts = require('../models/Workouts');
 const router  = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/all', (req, res, next) => {
+  
+  Workouts.find({}).then(workouts => {
+    if(!workouts){
+    
+      res.status(500).json({ message: `No workouts found in database` });
+    }
+  })
 });
 
 module.exports = router;
