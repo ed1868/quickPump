20fb41909e2a85fcb7ff08100340ebc4493b6348
eddieruiz <eddi@overseasinternational.com>
2019-11-26 10:55:07 -0500

    wut

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Schedule = require("../models/Schedule");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+////////////////////////////// route to pull all schedule from DB //////////////////////
+
+router.get("/", (req, res, next) => {
+  Schedule.find({})
+    .then(schedule => {
+      console.log(`WHOLE SCHEDULE IN OUR DATABASE ---- ${schedule}`);
+
+      if (!schedule) {
+        console.log("THERE WAS A PROBLEM GETTING SCHEDULE");
+      }
+    })
+    .catch(err => {
+      if (err) {
+        console.log(err);
+      }
+    });
 });
 
 module.exports = router;
